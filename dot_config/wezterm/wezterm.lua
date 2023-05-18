b68725858ec6cfa28cae278431c1dc72fbf1ca49
Alex Campos <alex.campos@gmail.com>
2023-05-17 21:46:47 -0500

    working on wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,9 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
 -- This table will hold the configuration.
 local config = {}
-
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
@@ -11,20 +9,59 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
---[[ config.color_scheme = "AdventureTime" ]]
---[[ config.color_scheme = "Batman" ]]
+-- config.color_scheme = "AdventureTime"
+-- config.color_scheme = "Batman"
+-- config.color_scheme = "AdventureTime"
 color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 12
+config.cell_width = 0.9
 config.window_background_opacity = 0.9
---[[ font_size = 11.0 ]]
---[[ allow_square_glyphs_to_overflow_width = "Always" ]]
---[[ config.font = wezterm.font { ]]
---[[   family = 'FiraCode Nerd Font', ]]
---[[ } ]]
---[[ cell_width = 1.2 ]]
---[[ wezterm.font 'FiraCode Nerd Font' ]]
---config.font = westerm.font "FiraCode Nerd Font"
+config.initial_cols = 140
+config.initial_rows = 40
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+config.enable_scroll_bar = true
+config.scrollback_lines = 5000
+config.hyperlink_rules = {
+	-- Linkify things that look like URLs and the host has a TLD name.
+	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+	{
+		regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+		format = "$0",
+	},
+
+	-- linkify email addresses
+	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+	{
+		regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+		format = "mailto:$0",
+	},
+
+	-- file:// URI
+	-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+	{
+		regex = [[\bfile://\S*\b]],
+		format = "$0",
+	},
+
+	-- Linkify things that look like URLs with numeric addresses as hosts.
+	-- E.g. http://127.0.0.1:8000 for a local development server,
+	-- or http://192.168.1.1 for the web interface of many routers.
+	{
+		regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+		format = "$0",
+	},
+
+	-- Make username/project paths clickable. This implies paths like the following are for GitHub.
+	-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+	-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+	{
+		regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+		format = "https://www.github.com/$1/$3",
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
