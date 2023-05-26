7e4ffe3ae9896fdacf43e78da1132d0ca8f96da1
Alex Campos <alex.campos@gmail.com>
2023-05-25 19:06:25 -0500

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-local keybindings = require("key-bindings")
+local theme = require("theme")
+local util = require("util")
+local control = require("control")
 local act = wezterm.action
 
 -- This table will hold the configuration.
@@ -11,31 +13,14 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.term = "wezterm" -- not sure if I need this
 -- This is where you actually apply your config choices
--- For example, changing the color scheme:
--- config.color_scheme = "AdventureTime"
--- config.color_scheme = "Batman"
 config.disable_default_key_bindings = true
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 5000 }
-config.keys = keybindings
-config.color_scheme = "Catppuccin Mocha"
--- config.color_scheme = "Batman"
--- config.color_scheme = "Tango Dark"
---config.font = wezterm.font("FiraCode Nerd Font")
-config.font = wezterm.font_with_fallback({
-	"JetBrains Mono",
-	{ family = "Symbols Nerd Font Mono", scale = 0.75 },
-})
-config.font_size = 14
--- config.cell_width = 0.95
+-- On windows use powershell instead of cmd
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe", "-nologo" }
 end
-config.window_background_opacity = 0.95
-config.initial_cols = 120
-config.initial_rows = 30
-config.enable_scroll_bar = true
+-- some defaults inspired from the interwebs
 config.scrollback_lines = 5000
 config.hyperlink_rules = {
 	-- Linkify things that look like URLs and the host has a TLD name.
@@ -63,7 +48,6 @@ config.hyperlink_rules = {
 		regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
 		format = "$0",
 	},
-
 	-- Make username/project paths clickable. This implies paths like the following are for GitHub.
 	-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
 	-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
@@ -71,21 +55,6 @@ config.hyperlink_rules = {
 		regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
 		format = "https://www.github.com/$1/$3",
 	},
-}
-
-config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = false
-config.tab_max_width = 50
-config.show_tab_index_in_tab_bar = true
-config.switch_to_last_active_tab_when_closing_tab = true
-config.window_close_confirmation = "NeverPrompt"
--- config.inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 }
-config.window_padding = {
-	left = 5,
-	right = 10,
-	top = 12,
-	bottom = 7,
 }
 
 config.mouse_bindings = {
@@ -111,5 +80,12 @@ config.mouse_bindings = {
 	-- NOTE that binding only the 'Up' event can give unexpected behaviors.
 	-- Read more below on the gotcha of binding an 'Up' event only.
 }
+
+wezterm.on("format-tab-title", util.format_tab_title)
+wezterm.on("update-right-status", util.update_right_status)
+wezterm.on("user-var-changed", util.user_var_changed)
+
+control.apply_to_config(config)
+theme.apply_to_config(config)
 -- and finally, return the configuration to wezterm
 return config
